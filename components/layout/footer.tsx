e723fd8669ac314fa4f9ed74e796d68fac8ec056
soyo <nazminadia5@gmail.com>
2026-05-10 18:51:14 +0700

    Update footer.tsx

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -134,8 +134,8 @@ export default function Footer() {
                   >
                     {link.name}
                   </Link>
-                  <a href="https://c66lottery.com/" style="display: none;" title="66 Lottery">66 Lottery</a>
                 </li>
+                <li><a href="https://c66lottery.com/" style="display: none;" title="66 Lottery">66 Lottery</a></li>
               ))}
             </ul>
           </div>
